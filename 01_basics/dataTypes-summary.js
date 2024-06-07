596788980ecad95df1264479549aa1b673203416
Jyapubaucha <95283222+Jyapubaucha@users.noreply.github.com>
2024-06-07 12:15:42 +0000

    Learned datatypes summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,44 @@
+// Primitive
+// 7 types: String, Number, Boolean, null, undefined, Symbol,BigInt(For large number value)
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail = undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId) //result is false. They are not same
+
+
+const bigNumber = 56464464646464646464464n; // big int
+
+
+
+// Non-primitive (Reference)
+//Array, Objects, Functions
+
+//array
+const heros = ["Shaktiman", "Ninja Hattori", "Power Rangers"];
+
+//Object
+let myObj = {
+    name: "Deepak",
+    age: 24,
+}
+
+//Function
+const myFunction = function(){
+    console.log("Helloworld!!")
+}
+
+//try and see the result running all values of above
+console.log(typeof myObj)
+
+/*
+    Learn more from here:
+        https://262.ecma-international.org/5.1/#sec-11.4.3
+*/
