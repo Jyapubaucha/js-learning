d562a37c41a1c0c2ba777e4578d959240f8a660b
Jyapubaucha <95283222+Jyapubaucha@users.noreply.github.com>
2024-06-07 10:46:08 +0000

    Conversion operation in more details.

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,5 +19,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn) //conversion to boolean
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******************* Operations *******************
+let value = 3;
+let negValue = -value
+// console.log(negValue) // -3
+// console.log(typeof negValue) //number
+
+// console.log(2+2) // => 4
+// console.log(2-2) // => 0
+// console.log(2*2) // => 4
+// console.log(2**3) // => 8
+// console.log(2/2) // => 1
+// console.log(2%2) // => 0 (reminder)
+
+let str1 = "Hello"
+let str2 = " Deepak"
+let str3 = str1 + str2
+// console.log(str3) // str3 => Hello Deepak
+
+// console.log(true)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
+
+/*
+    More to learn here:
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+*/
