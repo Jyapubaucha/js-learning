cfb0cd3b86ffa1c0b77c000f7f00a931eb71c9f6
Jyapubaucha <95283222+Jyapubaucha@users.noreply.github.com>
2024-06-07 11:04:02 +0000

    Learned comparision of datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+//null and undefined value can make confuse
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+/* 
+    == => just compare between values
+    === => compare value as well as their datatypes too
+*/
+console.log("2" === 2) //result => false
+
