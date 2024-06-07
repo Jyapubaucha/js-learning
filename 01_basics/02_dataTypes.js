0ec50dc7bd470e4995882c33b6c16312fca42c97
Jyapubaucha <95283222+Jyapubaucha@users.noreply.github.com>
2024-06-07 10:04:17 +0000

    Learned datatypes.

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";  //treat all JS code as  newer version
+
+// alert(3+3);  //we are using node js so it shows error
+
+console.log(3+3) //code readability should be high
+console.log("Deepak")
+
+let name = "Deepak" //Datatype: string
+let age = 24 //Datatype: number
+let isLoggedIn = false //Datatype: boolean
+
+/* 
+    Types of Datatypes
+    number => 2 to power 53
+    bigInt => mostly used in stock market
+    string => ""
+    boolean => true or false
+    null = > standalone value
+    undefined =>
+    symbol => unique
+
+    object
+
+*/
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
