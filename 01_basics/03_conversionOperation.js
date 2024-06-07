9fadc5cf47e1bf087a58bb683837149eb66d30ae
Jyapubaucha <95283222+Jyapubaucha@users.noreply.github.com>
2024-06-07 10:22:33 +0000

    Conversions of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33as"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score) //conversion to number
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+/* 
+    "33" => 33
+    "33as" => NaN
+    true => 1; false => 0; 
+*/
+
+let isLoggedIn = 1; //1 => true, 0 => false, "Deepak" => true
+let booleanIsLoggedIn = Boolean(isLoggedIn) //conversion to boolean
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
