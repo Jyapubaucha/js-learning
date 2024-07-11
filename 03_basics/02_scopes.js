24454489846570153b5ff1ad0bb83ca0e7919169
Jyapubaucha <95283222+Jyapubaucha@users.noreply.github.com>
2024-07-11 12:56:35 +0000

    Learned scope level and mini hoisting.

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,11 +9,53 @@ if(true){
     let a = 10
     const b = 20
     // var c = 30
-    console.log("Value of A is : ", a)
-    console.log("Value of B is : ", b)
+    // console.log("Value of A is : ", a)
+    // console.log("Value of B is : ", b)
 }
 
 
-console.log(a)
-console.log(b)
+// console.log(a)
+// console.log(b)
 // console.log(c)
+
+//Parent scope can be logged by child
+//But child scope cannot be logged by parent scope
+function one(){
+    const username = "Deepak"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true){
+    const username = "Deepak"
+    if(username === "Deepak"){
+        const website = " youtube"
+        // console.log(username + website)
+    }
+    // console.log(website);
+}
+//console.log(username)
+
+// ================================= //
+console.log(oneAdd(5));
+function oneAdd(num){
+    return num + 1;
+}
+
+
+// =================== Hoisting
+// Hoisting log the value after the variable declaration not before variable declaration
+
+// console.log(addTwo(5)) //Error occurs because the function is executed before variable declaration
+
+// declared addTwo as variable as well as functions too
+const addTwo = function(num){
+    return num + 2;
+}
+console.log(addTwo(5)) // This is a way to log the value
